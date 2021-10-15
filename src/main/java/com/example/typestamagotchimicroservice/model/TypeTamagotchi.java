55be6b8b1285cb66113b9c8e2941ab38f0894448
stenneyrinck <r0740487@student.thomasmore.be>
2021-10-15 14:57:27 +0200

    Added TamagotchiTypeClass, with getters and setters, 2 controllers, one empty, one with typename as parameter

--- src/main/java/com/example/typestamagotchimicroservice/model/TypeTamagotchi.java
+++ src/main/java/com/example/typestamagotchimicroservice/model/TypeTamagotchi.java
@@ -1,4 +1,93 @@
 package com.example.typestamagotchimicroservice.model;
 
+
+import javax.persistence.*;
+
+@Entity
 public class TypeTamagotchi {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private int id;
+
+    @Column(unique=true)
+    private String typeName;
+    private String description;
+    private int maxWeight;
+    private int minWeight;
+    private int minHealth;
+    private int neuroticism;
+    private int metabolism;
+    private int minHappiness;
+
+    public TypeTamagotchi() {
+    }
+
+    public TypeTamagotchi(String typeName) {
+        this.typeName = typeName;
+    }
+
+    public String getTypeName() {
+        return typeName;
+    }
+
+    public void setTypeName(String typeName) {
+        this.typeName = typeName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getMaxWeight() {
+        return maxWeight;
+    }
+
+    public void setMaxWeight(int maxWeight) {
+        this.maxWeight = maxWeight;
+    }
+
+    public int getMinWeight() {
+        return minWeight;
+    }
+
+    public void setMinWeight(int minWeight) {
+        this.minWeight = minWeight;
+    }
+
+    public int getMinHealth() {
+        return minHealth;
+    }
+
+    public void setMinHealth(int minHealth) {
+        this.minHealth = minHealth;
+    }
+
+    public int getNeuroticism() {
+        return neuroticism;
+    }
+
+    public void setNeuroticism(int neuroticism) {
+        this.neuroticism = neuroticism;
+    }
+
+    public int getMetabolism() {
+        return metabolism;
+    }
+
+    public void setMetabolism(int metabolism) {
+        this.metabolism = metabolism;
+    }
+
+    public int getMinHappiness() {
+        return minHappiness;
+    }
+
+    public void setMinHappiness(int minHappiness) {
+        this.minHappiness = minHappiness;
+    }
 }
