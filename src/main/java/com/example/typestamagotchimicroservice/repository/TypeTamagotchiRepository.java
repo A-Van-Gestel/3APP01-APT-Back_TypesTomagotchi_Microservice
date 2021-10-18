1814486beb199f87944dceb9ceb25fe5e4c25bb0
stenneyrinck <r0740487@student.thomasmore.be>
2021-10-18 15:39:31 +0200

    Added unit tests

--- src/main/java/com/example/typestamagotchimicroservice/repository/TypeTamagotchiRepository.java
+++ src/main/java/com/example/typestamagotchimicroservice/repository/TypeTamagotchiRepository.java
@@ -5,6 +5,12 @@ import com.example.typestamagotchimicroservice.model.TypeTamagotchi;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TypeTamagotchiRepository extends JpaRepository<TypeTamagotchi, Integer> {
+    List<TypeTamagotchi> findTypeTamagotchis();
+    TypeTamagotchi findTypeTamagotchiById(int id);
+    TypeTamagotchi findTypeTamagotchiByTypeName(String typeName);
+    List<TypeTamagotchi> findTypeTamagotchisByTypeNameContaining(String typeName);
 }
